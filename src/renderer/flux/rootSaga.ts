c165ee6074c12ad392fc190e9b0a2fc8e964a90f
Rodion <rodion.l@cronix.ms>
2018-05-21 18:30:24 +0300

    refactor(settings): add preloader and display profile fields

--- src/renderer/flux/rootSaga.ts
+++ src/renderer/flux/rootSaga.ts
@@ -3,7 +3,7 @@ import {fork} from 'redux-saga/effects';
 import { menuItem } from 'src/renderer/component/Menu/flux/menuItemSaga';
 import { createAccountSaga } from 'src/renderer/component/Auth/flux/CreateAccountSaga';
 import { loginSaga, watcherSetToken } from 'src/renderer/component/Auth/flux/LoginSaga';
-import { watcherResetPassword } from '../component/Account/flux/saga';
+import { watcherResetPassword, watcherGetProfile } from '../component/Account/flux/saga';
 
 export  default function* rootSaga () {
   yield [
@@ -12,5 +12,6 @@ export  default function* rootSaga () {
     fork(menuItem),
     fork(watcherSetToken),
     fork(watcherResetPassword),
+    fork(watcherGetProfile)
   ];
 }
