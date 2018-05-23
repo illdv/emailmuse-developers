173754ef07739782d4283693304bac673d0b5f3e
Dmitry Uskov <uskov.d@cronix.ms>
2018-05-23 10:52:37 +0300

    fix(image library): fix sagas to stay at the current page after getImages request

--- src/renderer/component/ImageLibrary/store/sagas/updateImageSaga.ts
+++ src/renderer/component/ImageLibrary/store/sagas/updateImageSaga.ts
@@ -1,14 +1,17 @@
-import { call, put, take } from 'redux-saga/effects';
+import { call, put, take, select } from 'redux-saga/effects';
 import * as constants from 'src/renderer/component/ImageLibrary/store/constants';
 import * as actions from 'src/renderer/component/ImageLibrary/store/actions';
 import * as EmailerAPI from 'src/renderer/API/EmailerAPI';
 import { IActionPayload } from 'src/renderer/flux/utils';
+import { getCurrentPageSelector } from 'src/renderer/component/ImageLibrary/store/selectors';
 
 function* updateImageWorker(action: IActionPayload<{ imageId: number, name: string }>): IterableIterator<any> {
   try {
     const response = yield call(EmailerAPI.ImageLibrary.updateImage, action.payload.imageId, action.payload.name);
     yield put(actions.updateImageSuccess());
-    yield put(actions.getImagesRequest());
+    const currentPage = yield select(getCurrentPageSelector);
+    // Checks for currentPage being correct are implemented in getImagesWorker saga
+    yield put(actions.getImagesRequest(currentPage));
   } catch (e) {
     console.log('Updating image failed: ', e);
     yield put(actions.updateImageFailure());
