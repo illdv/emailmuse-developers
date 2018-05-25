9e687a2143430a4f5832a94319259151ae79b7de
Nikita <kamyshenko.n@cronix.ms>
2018-05-25 18:47:49 +0300

    feat(Template) add edit title and description

--- src/renderer/component/Templates/TemplateEditor.tsx
+++ src/renderer/component/Templates/TemplateEditor.tsx
@@ -1,15 +1,22 @@
 import * as React from 'react';
-import { Component } from 'react';
+import { ChangeEvent, Component } from 'react';
 import { Close, Delete, Save } from '@material-ui/icons';
+import block from 'bem-ts';
 
 import { JoditEditor } from 'src/renderer/common/Jodit/JoditEditor';
 import { ITemplate } from 'src/renderer/component/Templates/flux/models';
 import { Fab } from 'src/renderer/common/Fab';
+import { Grid, TextField } from '@material-ui/core';
+import './TemplateEditor.scss';
+
+const b = block('template-editor');
 
 export namespace TemplateEditorSpace {
   export interface IState {
     templateId: number;
     value: string;
+    title: string;
+    description: string;
   }
 
   export interface IProps {
@@ -22,7 +29,7 @@ export namespace TemplateEditorSpace {
 
 export class TemplateEditor extends Component<TemplateEditorSpace.IProps, TemplateEditorSpace.IState> {
 
-  state: TemplateEditorSpace.IState = { value: '', templateId: -1 };
+  state: TemplateEditorSpace.IState = { value: '', templateId: -1, description: '', title: '' };
 
   static getDerivedStateFromProps(
     nextProps: TemplateEditorSpace.IProps,
@@ -33,6 +40,8 @@ export class TemplateEditor extends Component<TemplateEditorSpace.IProps, Templa
       return {
         templateId: template.id,
         value: template.body,
+        title: template.title,
+        description: template.description || '',
       };
 
     }
@@ -49,9 +58,18 @@ export class TemplateEditor extends Component<TemplateEditorSpace.IProps, Templa
     });
   }
 
+  onChangeField = (event: ChangeEvent<HTMLInputElement>) => {
+    const { id, value } = event.target;
+    this.setState({ [id]: value } as any);
+  }
+
   onSave = () => {
-    const template = this.props.template;
-    template.body  = this.state.value;
+    const template                      = this.props.template;
+    const { value, title, description } = this.state;
+
+    template.body        = value;
+    template.title       = title;
+    template.description = description;
     this.props.save(template);
   }
 
@@ -59,6 +77,25 @@ export class TemplateEditor extends Component<TemplateEditorSpace.IProps, Templa
     const { close, remove } = this.props;
     return (
       <>
+        <Grid item xs={12}>
+          <TextField
+            className={b('text-field')}
+            id='title'
+            label='Title'
+            margin='normal'
+            value={this.state.title}
+            onChange={this.onChangeField}
+          />
+          <TextField
+            multiline
+            className={b('text-field')}
+            id='description'
+            label='Description'
+            margin='normal'
+            value={this.state.description}
+            onChange={this.onChangeField}
+          />
+        </Grid>
         <JoditEditor onChangeValue={this.onChange} value={this.state.value}/>
         <div>
           <Fab
