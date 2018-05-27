ccd0310fc9139415465fe360d6af449332b0c2f5
Nikita Kamyshenko <kamyshenko.n@cronix.ms>
2018-05-28 00:43:55 +0300

    fix(*) show error if failed send code

--- src/renderer/component/Authorization/flux/SendCodeOnMailSaga.ts
+++ src/renderer/component/Authorization/flux/SendCodeOnMailSaga.ts
@@ -1,7 +1,7 @@
 import { call, put, take } from 'redux-saga/effects';
 import { FluxAccounts } from 'src/renderer/component/Authorization/flux/FluxAccounts';
 import { FluxValidation } from 'src/renderer/common/Validation/flux/actions';
-import { FluxToast } from 'src/renderer/common/Toast/flux/actions';
+import { FluxToast, ToastType } from 'src/renderer/common/Toast/flux/actions';
 import { sendCodeOnMail } from '../../../API/Auth';
 
 function* onSendCodeOnMail(action: { payload: { email: string } }): IterableIterator<any> {
@@ -10,7 +10,8 @@ function* onSendCodeOnMail(action: { payload: { email: string } }): IterableIter
     yield sendCodeOnMail(action.payload.email);
     yield put(FluxAccounts.Actions.ForgotPassword.sendCodeOnMail.SUCCESS());
   } catch (error) {
-    yield put(FluxToast.Actions.showToast(error.response.data.message));
+    yield put(FluxToast.Actions.showToast(error.response.data.message, ToastType.Error));
+    yield put(FluxAccounts.Actions.ForgotPassword.sendCodeOnMail.FAILURE());
   }
 }
 
