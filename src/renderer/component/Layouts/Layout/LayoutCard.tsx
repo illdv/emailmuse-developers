f1ab4713ce4af08558819cbbd37a68170f9012be
M.Roman <moshchenkov.r@cronix.ms>
2018-06-27 12:01:01 +0300

    feat(Layout) add delete, edit icons

--- src/renderer/component/Layouts/Layout/LayoutCard.tsx
+++ src/renderer/component/Layouts/Layout/LayoutCard.tsx
@@ -1,25 +1,48 @@
 import * as React from 'react';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import Typography from '@material-ui/core/Typography';
 import block from 'bem-ts';
+import { Button, Card, CardContent, IconButton, Typography, withStyles } from '@material-ui/core';
+import { Delete, Edit } from '@material-ui/icons';
+
 import './LayoutCard.scss';
+
 const b = block('layout-card');
 
+const styles = theme => ({
+  button: {
+    margin: theme.spacing.unit,
+    rightIcon: {
+      marginLeft: theme.spacing.unit,
+    },
+    padding: '2px',
+  },
+  input: {
+    display: 'none',
+  },
+});
+
 const LayoutCard = props => {
-  const { cardTitle, children, onClick } = props;
+  const { cardTitle, children, onClick, classes } = props;
   return (
     <Card className={b()} onClick={onClick}>
+      <div className={b('action_button')}>
+        <IconButton className={classes.button} aria-label='delete template'>
+          <Delete className={classes.rightIcon} />
+        </IconButton>
+        <IconButton className={classes.button}>
+          <Edit className={classes.rightIcon} aria-label='edit template' />
+        </IconButton>
+      </div>
       <CardContent>
-          <Typography component='p' align='center'>
-            {children}
-          </Typography>
+        <Typography component='p' align='center'>
+          {children}
+        </Typography>
       </CardContent>
       <Typography variant='headline' align='center'>
         {cardTitle ? cardTitle : null}
       </Typography>
+
     </Card>
-  );
+);
 };
 
-export default LayoutCard;
+export default withStyles(styles)(LayoutCard);
