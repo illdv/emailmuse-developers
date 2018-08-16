525a5943d6c11450ec356594d2ec967f8b44ef28
M.Roman <moshchenkov.r@cronix.ms>
2018-08-16 19:44:06 +0300

    feat(Emails) Fix DnD

--- src/renderer/component/Emails/NodeList/NodeTableFolderEmail.tsx
+++ src/renderer/component/Emails/NodeList/NodeTableFolderEmail.tsx
@@ -5,7 +5,6 @@ import { DropTarget, DragSource } from 'react-dnd';
 import block from 'bem-ts';
 
 const b = block('node_table_custom_row');
-
 import { IFolderEmail, nodeType } from 'src/renderer/component/Emails/flux/interfaceAPI';
 
 interface INodeTableRow<T> {
@@ -24,71 +23,68 @@ export const INodeTypes = {
   FOLDER: 'folder',
 };
 
-const NodeTableFolderEmail: React.SFC<INodeTableRow<any>> = (
-  {
-    connectDropTarget, connectDragSource, isDragging,
-    item, onDeleteFolder, onOpenItem, onUpdateEmail,
-  }) => {
-  const isFolder = item.type === nodeType.folder;
-
-  const handleOpen = () => onOpenItem(item);
-  const handleDelete = () => onDeleteFolder(item);
-
-  return connectDragSource(
-    connectDropTarget(
-      <tr
-        role='checkbox'
-        // aria-checked={isSelected}
-        tabIndex={-1}
-        // selected={isSelected}
-        className={b()}
-        style={{ opacity: isDragging ? 0.5 : 1 }}
-      >
-        <TableCell
-          style={{ width: 40 }}
-          // onClick={handleCopy(item.id)}
-          padding={'checkbox'}
-        >
-          <IconButton onClick={handleOpen}>
-            {isFolder ? <Folder/> : <Email/>}
-          </IconButton>
-        </TableCell>
-
-        <TableCell component='th' scope='row' padding='none' onClick={handleOpen}>
-          {item.title}
-        </TableCell>
-        <TableCell>
-          {isFolder ? 'Folder' : item.description}
-        </TableCell>
-        <TableCell>
-          {item.updated_at || '---'}
-        </TableCell>
-        <TableCell>
-          {isFolder ? <IconButton onClick={handleDelete}><Delete/></IconButton> : null}
-        </TableCell>
-      </tr>),
-  );
-};
-
 const NodeTarget = {
   drop({ item, onUpdateEmail }, monitor) {
-    console.log('Target folder =>', item);
-    const targetEmail = monitor.getItem();
-    if (item.type === nodeType.folder) {
-      onUpdateEmail({ id: targetEmail.itemId, folder_id: item.id });
-    } else {
-      console.log('(item.type = email =>');
+    const targetEmail = monitor.getItem().item;
+    if (targetEmail.type === nodeType.email) {
+      onUpdateEmail({ id: targetEmail.id, folder_id: item.id });
     }
   },
 };
 
 const NodeSource = {
-  beginDrag(props) {
-    return {
-      itemId: props.item.id,
-    };
+  beginDrag({ item }) {
+    return { item };
   },
 };
+
+@DropTarget(INodeTypes.EMAIL, NodeTarget, collectTarget)
+@DragSource(INodeTypes.EMAIL, NodeSource, collectSource)
+export class NodeTableFolderEmail extends React.Component<any, any> {
+  isFolder = this.props.item.type === nodeType.folder;
+
+  handleOpen = () => this.props.onOpenItem(this.props.item);
+  handleDelete = () => this.props.onDeleteFolder(this.props.item);
+
+  render() {
+    const { connectDropTarget, connectDragSource, isDragging, item } = this.props;
+    return connectDragSource(
+      connectDropTarget(
+        <tr
+          role='checkbox'
+          // aria-checked={isSelected}
+          tabIndex={-1}
+          // selected={isSelected}
+          className={b()}
+          style={{ opacity: isDragging ? 0.5 : 1 }}
+        >
+          <TableCell
+            style={{ width: 40 }}
+            // onClick={handleCopy(item.id)}
+            padding={'checkbox'}
+          >
+            <IconButton onClick={this.handleOpen}>
+              {this.isFolder ? <Folder/> : <Email/>}
+            </IconButton>
+          </TableCell>
+
+          <TableCell component='th' scope='row' padding='none' onClick={this.handleOpen}>
+            {item.title}
+          </TableCell>
+          <TableCell>
+            {this.isFolder ? 'Folder' : item.description}
+          </TableCell>
+          <TableCell>
+            {item.updated_at || '---'}
+          </TableCell>
+          <TableCell>
+            {this.isFolder ? <IconButton onClick={this.handleDelete}><Delete/></IconButton> : null}
+          </TableCell>
+        </tr>),
+    );
+  }
+
+}
 
 function collectTarget(connect, monitor) {
   return {
@@ -103,7 +99,3 @@ function collectSource(connect, monitor) {
     isDragging: monitor.isDragging(),
   };
 }
-
-export default DragSource(INodeTypes.FOLDER, NodeSource, collectSource)(
-  DropTarget(INodeTypes.EMAIL, NodeTarget, collectTarget)(NodeTableFolderEmail),
-);
