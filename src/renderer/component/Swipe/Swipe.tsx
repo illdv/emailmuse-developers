3abc35c14f3fdf71eb2af92bb8ab9e0b7df67ee8
slavka <slava.sinakov@yandex.ru>
2018-10-18 13:26:39 +0300

    fix: swipe layout text

--- src/renderer/component/Swipe/Swipe.tsx
+++ src/renderer/component/Swipe/Swipe.tsx
@@ -101,7 +101,7 @@ export class Swipe extends Component<SwipeSpace.IProps & WithStyles<any>, SwipeS
 
     const items = [
       {
-        title: 'Swipes',
+        title: 'Swipe Vault',
         onClick: this.onResetSelect,
       },
     ];
