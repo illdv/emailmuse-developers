f602c2748c7a2d2418903b480cd7739979ea6d41
Dvoynikov Kirill <illdv@mail.ru>
2018-12-02 20:04:42 +0300

    add controlled electron-updater

--- src/main/index.ts
+++ src/main/index.ts
@@ -4,6 +4,7 @@ const {
   BrowserWindow,
   Menu,
   ipcMain,
+  ipcRenderer,
   shell,
   dialog,
 } = require('electron');
@@ -12,11 +13,9 @@ const { autoUpdater } = require('electron-updater');
 const path = require('path');
 const urlFormat = require('url');
 const loadDevTool = require('electron-load-devtool');
-
 let mainWindow;
 
 let isProduction = false;
-
 try {
   isProduction = IS_PRODUCTION;
 } catch (e) {
@@ -57,12 +56,32 @@ autoUpdater.logger.transports.file.level = 'info';
 log.info('App starting...');
 
 function sendStatusToWindow(text) {
-  log.info(text);
+  // log.info(text);
   mainWindow.webContents.send('message', text);
 }
 
-autoUpdater.on('checking-for-update', () => {
+let updater;
+autoUpdater.autoDownload = false;
+
+autoUpdater.on('update-available', () => {
   sendStatusToWindow('Checking for update...');
+
+  dialog.showMessageBox(
+    mainWindow,
+    {
+      type: 'info',
+      title: 'Found Updates',
+      message: 'Found updates, do you want update now?',
+      buttons: ['Sure', 'No'],
+    },
+    buttonIndex => {
+      if (buttonIndex === 0) {
+        autoUpdater.downloadUpdate();
+      } else {
+        updater = null;
+      }
+    },
+  );
 });
 autoUpdater.on('update-available', info => {
   sendStatusToWindow('Update available.');
@@ -74,15 +93,28 @@ autoUpdater.on('error', err => {
   sendStatusToWindow('Error in auto-updater. ' + err);
 });
 autoUpdater.on('download-progress', progressObj => {
-  let logMessage = 'Download speed: ' + progressObj.bytesPerSecond;
-  logMessage =
-    logMessage + ' - Downloaded ' + Math.floor(progressObj.percent) + '%';
-  logMessage =
-    logMessage + ' (' + progressObj.transferred + '/' + progressObj.total + ')';
+  const logMessage = ' - Downloaded ' + Math.floor(progressObj.percent) + '%';
+  // logMessage =
+  //   logMessage + ' (' + progressObj.transferred + '/' + progressObj.total + ')';
   sendStatusToWindow(logMessage);
 });
-autoUpdater.on('update-downloaded', info => {
+
+autoUpdater.on('update-downloaded', () => {
   sendStatusToWindow('Update downloaded');
+
+  dialog.showMessageBox(
+    mainWindow,
+    {
+      title: 'Install Updates',
+      message: 'Updates downloaded, application will be quit for update...',
+      buttons: ['Reset', 'No'],
+    },
+    buttonIndex => {
+      if (buttonIndex === 0) {
+        autoUpdater.quitAndInstall();
+      }
+    },
+  );
 });
 
 app.on('activate', () => {
@@ -91,13 +123,28 @@ app.on('activate', () => {
   }
 });
 
+function dialogWarningClose(window) {
+  window.on('close', e => {
+    const warningClose = dialog.showMessageBox(window, {
+      type: 'warning',
+      message:
+        'Аpplication update is running, you are sure you want to close it?',
+      defaultId: 1,
+      buttons: ['Yes', 'No'],
+    });
+    if (warningClose === 1) {
+      e.preventDefault();
+    }
+  });
+}
+
 app.on('ready', () => {
   createWindow();
   if (process.platform === 'darwin') {
     createMenuForMac();
   }
   if (isProduction) {
-    autoUpdater.checkForUpdatesAndNotify();
+    autoUpdater.checkForUpdates();
   }
 });
 
@@ -128,48 +175,56 @@ function createMenuForMac() {
   );
 }
 
-ipcMain.on('authorized-google', (e, url) => {
-  const loginWindow = new BrowserWindow({
-    webPreferences: { nodeIntegration: false },
+function authorizedGoogle() {
+  ipcMain.on('authorized-google', (e, url) => {
+    const loginWindow = new BrowserWindow({
+      webPreferences: { nodeIntegration: false },
+      parent: mainWindow,
+      modal: true,
+      show: false,
+    });
+    loginWindow.show();
+    loginWindow.loadURL(url);
+    const ses = mainWindow.webContents.session;
+    // loginWindow.webContents.on('will-navigate', (event, newUrl) => {
+    //   extractResponseFromPage(newUrl, loginWindow);
+    // });
+
+    ses.webRequest.onBeforeRequest({ urls: [] }, (details, callback) => {
+      if (!details.url.includes('app.emailmuse.com')) {
+        extractResponseFromPage(details.url, loginWindow);
+      }
+      callback({});
+    });
+
+    // loginWindow.webContents.on(
+    //   'did-get-redirect-request',
+    //   (event, oldUrl, newUrl) => {
+    //     // console.log(newUrl);
+
+    //     extractResponseFromPage(newUrl, loginWindow);
+    //   },
+    // );
+    ses.clearStorageData({});
   });
-  loginWindow.loadURL(url);
-  const ses = mainWindow.webContents.session;
-  // loginWindow.webContents.on('will-navigate', (event, newUrl) => {
-  //   extractResponseFromPage(newUrl, loginWindow);
-  // });
 
-  ses.webRequest.onBeforeRequest({ urls: [] }, (details, callback) => {
-    if (!details.url.includes('app.emailmuse.com')) {
-      extractResponseFromPage(details.url, loginWindow);
-    }
-    callback({});
-  });
-
-  // loginWindow.webContents.on(
-  //   'did-get-redirect-request',
-  //   (event, oldUrl, newUrl) => {
-  //     // console.log(newUrl);
-
-  //     extractResponseFromPage(newUrl, loginWindow);
-  //   },
-  // );
-  ses.clearStorageData({});
-});
-
-function extractResponseFromPage(url, loginWindow) {
-  const javaScript = `
-  function getUser() {
-    if(document.body.children.length === 1) {
-        var pre = document.querySelector('pre');
-        return pre ? pre.innerText : false;
-    }
-	  return false;
-   }   
-   getUser();`;
-  loginWindow.webContents.executeJavaScript(javaScript, result => {
-    if (result) {
-      mainWindow.webContents.send(`authorized-google-success`, result);
-      loginWindow.hide();
-    }
-  });
+  function extractResponseFromPage(url, loginWindow) {
+    const javaScript = `
+    function getUser() {
+      if(document.body.children.length === 1) {
+          var pre = document.querySelector('pre');
+          return pre ? pre.innerText : false;
+      }
+      return false;
+     }   
+     getUser();`;
+    loginWindow.webContents.executeJavaScript(javaScript, result => {
+      if (result) {
+        mainWindow.webContents.send(`authorized-google-success`, result);
+        loginWindow.hide();
+      }
+    });
+  }
 }
+
+authorizedGoogle();
